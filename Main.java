dcd849cf8f9694a3694bc7826a487ee77c69d9b4
repl.it user <replituser@example.com>
2020-10-23 10:12:28 +0000

    Assignment 1 Update

--- Main.java
+++ Main.java
@@ -2,21 +2,29 @@ class Main {
   public static void main(String[] args) {
     for(int i = 1; i< 201; i++){
       System.out.println(i);
-      durchFünfTeilbar(i);
-      endetAufNeun(i);
-      addiertMitVorherDurchDreiTeilbar(i); 
+      zahlenfilter(i);
+ 
     }
   }
-  public static void durchFünfTeilbar(int zahl){
+
+  public static void zahlenfilter(int zahl){
+      durchFuenfTeilbar(zahl);
+      endetAufNeun(zahl);
+      addiertMitVorherDurchDreiTeilbar(zahl);
+  }
+
+  public static void durchFuenfTeilbar(int zahl){
       if (zahl % 5 == 0){
           System.out.print("ZAHL ist durch 5 teilbar\n");
       }
   }
+
   public static void endetAufNeun(int zahl){
       if (zahl % 10 == 9 || zahl == 9){
           System.out.print("ZAHL endet auf 9\n");
       }
   }
+
   public static void addiertMitVorherDurchDreiTeilbar(int zahl){
       if((zahl + (zahl-1)) % 3 == 0){
           int ergebnis = zahl + (zahl-1);
